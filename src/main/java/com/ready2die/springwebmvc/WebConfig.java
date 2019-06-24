aa464b2e3801cfe39f4c191a30ac690fb17ece6b
Leesunghee <realwunz@gmail.com>
2019-06-24 22:39:06 +0900

    practice resource handler

--- src/main/java/com/ready2die/springwebmvc/WebConfig.java
+++ src/main/java/com/ready2die/springwebmvc/WebConfig.java
@@ -2,8 +2,12 @@ package com.ready2die.springwebmvc;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -20,5 +24,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new AnotherGreetingInterceptor())
                 .addPathPatterns("/hi")
                 .order(-1);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/")
+//                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+                .resourceChain(true);
+
     }
 }
--- src/test/java/com/ready2die/springwebmvc/SampleControllerTest.java
+++ src/test/java/com/ready2die/springwebmvc/SampleControllerTest.java
@@ -1,20 +1,24 @@
 package com.ready2die.springwebmvc;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.persistence.PersistenceContext;
+import javax.xml.ws.spi.http.HttpHandler;
 
+import static com.sun.tools.doclets.formats.html.markup.HtmlStyle.header;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -40,6 +44,23 @@ public class SampleControllerTest {
             .param("id", savedPerson.getId().toString()))
                 .andDo(print())
                 .andExpect(content().string("hello sunghee"));
+    }
+
+    @Test
+    public void helloStatic() throws Exception {
+        this.mockMvc.perform(get("/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello index")));
+    }
+
+    @Test
+    public void helloMobileStatic() throws Exception {
+        this.mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("Hello Mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
     }
 
 }
