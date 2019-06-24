0e28e9a3157a7eed5b5caa2efa0b070af6118bca
Leesunghee <realwunz@gmail.com>
2019-06-24 22:22:34 +0900

    implementation handler intercepter

--- src/main/java/com/ready2die/springwebmvc/SampleController.java
+++ src/main/java/com/ready2die/springwebmvc/SampleController.java
@@ -8,6 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    // preHandle1
+    // preHandle2
+    // 요청 처리
+    // postHandle2
+    // postHandle1
+    // 뷰 렌더링
+    // afterCompletion2
+    // afterCompletion1
+
     @GetMapping("/hello")
 //    public String hello(@PathVariable("name") Person person) {
     public String hello(@RequestParam("id") Person person) {
